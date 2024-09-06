3d1a1c6a277d9fdfb99d595d5ab5f1948807631d
Filip Žitný <filipzitny@gmail.com>
2024-09-06 11:41:45 +0200

    smaller setup for jetson

--- game/settings.py
+++ game/settings.py
@@ -65,3 +65,28 @@ def check_right_hand_movement():
 # Render the output (this can be called in the game loop to render the video)
 def render_output():
     output.Render(input.Capture())
+
+
+SCREEN_WIDTH = 864
+SCREEN_HEIGHT = 936
+FPS = 60
+
+# Colors
+WHITE = (255, 255, 255)
+
+# Font
+pygame.init()
+FONT = pygame.font.SysFont('Bauhaus 93', 60)
+
+# Game variables
+GROUND_SCROLL = 0
+SCROLL_SPEED = 4
+FLYING = False
+GAME_OVER = False
+PIPE_GAP = 350
+PIPE_FREQUENCY = 2000  # milliseconds
+
+# Load images
+BG = pygame.image.load('assets/bg.png')
+GROUND_IMG = pygame.image.load('assets/ground.png')
+BUTTON_IMG = pygame.image.load('assets/restart.png')
